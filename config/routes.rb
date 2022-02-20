ed754c5aa3e9bf36dc5be03eaf6e3011c6557a43
FM0120git <kaz.fumiaki@gmail.com>
2022-02-20 13:15:02 +0000

    update

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
   get 'top' => 'homes#top'
   post 'todolists' => 'todolists#create'
   get 'todolists' => 'todolists#index'
-  kkk
+  get 'todolists/:id' => 'todolists#show',as: 'todolist'
+  # # as: '名前' は「名前付きルート」といい、
+  # コード内でURLをわかりやすく書くために使われます。
 end
