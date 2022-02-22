ec508643553a6333a5d97bbec9780ab3c59fcf69
FM0120git <kaz.fumiaki@gmail.com>
2022-02-22 01:42:18 +0000

    update

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '321156ea73600d422b4c8ad6bd17d8d1aa0bbe72d84dfdcffd12ed100d5a1a696793f5e8645bee6ee33ca93dfc33048b0cf5cf80f3e2052e732f707b1e8a65f7'
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_19_015446) do
+ActiveRecord::Schema.define(version: 2022_02_22_002538) do
 
   create_table "lists", force: :cascade do |t|
     t.string "title"
     t.string "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image_id"
   end
 
 end
